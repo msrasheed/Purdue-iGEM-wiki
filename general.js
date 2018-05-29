ea9e9bb7eee9a862e1a7e4afa7a0dd5cb8e6e687
Julianne Dejoie <jdejoie@purdue.edu>
2018-05-29 15:07:34 -0400

    added folders and items

--- general.js
+++
@@ -1,5 +0,0 @@
-// window.onload = function () {
-//     console.log('hi');
-// 	console.log(document.getElementsByClassName('menu')[0].style.offset.bottom);
-// 	document.getElementsByClassName('nav-menu-side')[0].style.top = document.getElementsByClassName('menu')[0].style.bottom;
-// };
