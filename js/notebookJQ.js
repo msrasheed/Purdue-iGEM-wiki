5cd531f2dd5bbd274a5624bb72c8b3a3bb82a795
ninja-grass <moizr95@gmail.com>
2018-05-30 21:46:26 -0400

    kevin's face gets bigger

--- js/notebookJQ.js
+++ js/notebookJQ.js
@@ -52,4 +52,36 @@ $(document).ready(() => {
 	// $('.basic-page-side-menu').on('mouseleave', () => {
 	// 	$('.basic-page-side-menu').removeClass('slide-left-into-page');
 	// })
+
+	////////////////////////////////////////////////////
+	//For team page
+	////////////////////////////////////////////////////
+	let hasKevinPassed = false;
+	let countKevinPasses = 0;
+	let kevinTop = 0;
+	let kevinLeft = 0;
+	let kevinWidth = 235;
+	$(window).scroll(event => {
+		let scroll = $(window).scrollTop();
+		let $kevinDiv = $('.alt-sections').children()[1];
+		let kevinBenchmark = $($kevinDiv).offset().top + $($kevinDiv).outerHeight(true);
+		if(scroll > kevinBenchmark && !(hasKevinPassed)) {
+			hasKevinPassed = true;
+			console.log(countKevinPasses);
+			countKevinPasses++;
+			let $kevinImage = $($kevinDiv).find('img')[0];
+			kevinTop -= 50;
+			kevinLeft -= 50;
+			kevinWidth += 191.25;
+			if(countKevinPasses) {
+				$($kevinImage).css({
+					width:kevinWidth+'px',
+					top:kevinTop+'px',
+					left:kevinLeft+'px'
+				});
+			}
+		} else if (scroll < kevinBenchmark && hasKevinPassed) {
+			hasKevinPassed = false;
+		}
+	});
 });
