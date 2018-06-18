a3c8aa92ea6f2d8a552c1cd6e3c115870b555f18
Zachary Berglund <zberglun@purdue.edu>
2018-06-18 11:49:56 -0700

    Debugged navbtns
    Debugged the Nav Buttons for the bottom of the pages . They need styling so they can appear above the footer

--- js/navbtnsJS.js
+++ js/navbtnsJS.js
@@ -1,6 +1,5 @@
 function Main(){
 
-pages= setUpLibary();
 buttonCreation(pages);
 
 
@@ -8,14 +7,15 @@ buttonCreation(pages);
 
 
 }
-
+pages= setUpLibary();
+Main();
 
 function buttonCreation(sites){
 curPage = window.location.href;
 flag = 0;
 cnt = 0;
-for( i=0; i<sites.length();i++){
-      if(sites[i].url.uppercase()==curPage.uppercase()){
+for( i=0; i<sites.length;i++){
+      if(sites[i].url.toUpperCase()==curPage.toUpperCase()){
 	  flag++;
 	  cnt = i;
 	  }
@@ -27,18 +27,21 @@ if(flag){
   nxtBtn=document.createElement("button");
   prvBtn.innerHTML = "Previous Page";
   nxtBtn.innerHTML = "Next Page";
-  prvBtn.href = sites[cnt].previous_url;
-  nxtBtn.href = sites[cnt].next_url;
+  var prvUrl = "window.location.href="+sites[cnt].previous_url;
+  prvBtn.onclick=prvUrl;
+  prvBtn.addEventListener('click',function(){window.location.href=sites[cnt].previous_url;});
+  nxtBtn.addEventListener('click',function(){window.location.href=sites[cnt].next_url;});
   if(!(sites[cnt].previous_url=="Na")){
-  document.append(prvBtn);
+  document.getElementsByTagName("html")[0].appendChild(prvBtn);
   }
   if(!(sites[cnt].next_url=="Na")){
-  document.append(nxtBtn);
+  document.getElementsByTagName("html")[0].appendChild(nxtBtn);
   }
   if(flag >= 2){
-	  //Output the Link occurs twice in the libary
+	  //Output the Link occurs twice in the library
 	  
   }
+  
 }
 
 
@@ -48,22 +51,22 @@ if(flag){
 function setUpLibary(){
 lead ="http://2018.igem.org/Team:Purdue/";
 pages =[];
-pages.append(new setup(lead+"History",lead+"Team","Na"));
-pages.append(new setup(lead+"Team",lead+"Description",lead+"History"));
-pages.append(new setup(lead+"Description",lead+"Human_Practices",lead+"Team"));
-pages.append(new setup(lead+"Human_Practices",lead+"Genetic_Pathway",lead+"Description"));
-pages.append(new setup(lead+"Genetic_Pathway",lead+"Design",lead+"Human_Practices"));
-pages.append(new setup(lead+"Design",lead+"Safety",lead+"Genetic_Pathway"));
-pages.append(new setup(lead+"Safety",lead+"Experiments",lead+"Design"));
-pages.append(new setup(lead+"Experiments",lead+"Collaborations",lead+"Safety"));
-pages.append(new setup(lead+"Collaborations",lead+"Public_Engagement",lead+"Experiments"));
-pages.append(new setup(lead+"Public_Engagement",lead+"Applied_Model",lead+"Collaborations"));
-pages.append(new setup(lead+"Applied_Model",lead+"Hardware",lead+"Public_Engagement"));
-pages.append(new setup(lead+"Hardware",lead+"Results",lead+"Applied_Model"));
-pages.append(new setup(lead+"Results",lead+"Parts",lead+"Hardware"));
-pages.append(new setup(lead+"Parts",lead+"Model",lead+"Results"));
-pages.append(new setup(lead+"Model",lead+"Acknowledgement",lead+"Parts"));
-pages.append(new setup(lead+"Acknowledgement","http://igem.org/2018_Judging_Form?team=Purdue",lead+"Model"));
+pages.push(new setup(lead+"History",lead+"Team","Na"));
+pages.push(new setup(lead+"Team",lead+"Description",lead+"History"));
+pages.push(new setup(lead+"Description",lead+"Human_Practices",lead+"Team"));
+pages.push(new setup(lead+"Human_Practices",lead+"Genetic_Pathway",lead+"Description"));
+pages.push(new setup(lead+"Genetic_Pathway",lead+"Design",lead+"Human_Practices"));
+pages.push(new setup(lead+"Design",lead+"Safety",lead+"Genetic_Pathway"));
+pages.push(new setup(lead+"Safety",lead+"Experiments",lead+"Design"));
+pages.push(new setup(lead+"Experiments",lead+"Collaborations",lead+"Safety"));
+pages.push(new setup(lead+"Collaborations",lead+"Public_Engagement",lead+"Experiments"));
+pages.push(new setup(lead+"Public_Engagement",lead+"Applied_Model",lead+"Collaborations"));
+pages.push(new setup(lead+"Applied_Model",lead+"Hardware",lead+"Public_Engagement"));
+pages.push(new setup(lead+"Hardware",lead+"Results",lead+"Applied_Model"));
+pages.push(new setup(lead+"Results",lead+"Parts",lead+"Hardware"));
+pages.push(new setup(lead+"Parts",lead+"Model",lead+"Results"));
+pages.push(new setup(lead+"Model",lead+"Acknowledgement",lead+"Parts"));
+pages.push(new setup(lead+"Acknowledgement","http://igem.org/2018_Judging_Form?team=Purdue",lead+"Model"));
 // Add each page to this libary
 return pages
 
